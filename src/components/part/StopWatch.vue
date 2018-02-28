c1df659a9a4df6d3b80e198618bd9a638c19a234
高橋樹 <itsuki@takahashiju-no-MacBook-Pro.local>
2018-02-28 14:51:35 +0900

    add stopwatch

---
+++ src/components/part/StopWatch.vue
@@ -0,0 +1,20 @@
+<template lang='pug'>
+.vue-stop-watch
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+import VueUtil from '@/scripts/util/VueUtil';
+import BuefyVue from '@/components/base/BuefyVue';
+
+/**
+ * Vue Component
+ */
+@Component
+export default class StopWatch extends BuefyVue {
+}
+</script>
+
+<style lang='sass' scoped>
+.vue-stop-watch
+</style>
